726dff9090be657957b465b10181840be1b0af1e
Paul Baker <paul.n.baker@utah.edu>
2015-05-27 02:19:21 -0600

    File line iterable demo

--- src/main/java/IterableFileLineDemo.java
+++ src/main/java/IterableFileLineDemo.java
@@ -1,5 +1,46 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Iterator;
+import java.util.Scanner;
+
 /**
  * Created by paulbaker on 5/27/15.
  */
-public class IterableFileLineDemo {
+public class IterableFileLineDemo extends File implements Iterable<String> {
+    public IterableFileLineDemo(String pathname) {
+        super(pathname);
+    }
+
+    public Iterator<String> iterator() {
+        return new FileLineIterator(this);
+    }
+
+    private class FileLineIterator implements Iterator<String> {
+
+        final private Scanner scanner;
+
+        private FileLineIterator(File file) {
+            try {
+                scanner = new Scanner(file);
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public boolean hasNext() {
+            if (scanner.hasNextLine() == false) {
+                scanner.close();
+                return false;
+            }
+            return true;
+        }
+
+        public String next() {
+            return scanner.nextLine();
+        }
+    }
+
+    public static void main(String[] args){
+        
+    }
 }
