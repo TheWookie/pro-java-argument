9003055b08c508f4e753b928e6571dbf8d58098f
Paul Baker <paul.n.baker@utah.edu>
2015-05-27 00:33:15 -0600

    Adding a lot of demo that shows java isn't ENITRELY terrible.

---
+++ src/main/java/AutoCastDemo.java
@@ -0,0 +1,16 @@
+/**
+ * Created by paulbaker on 5/27/15.
+ */
+public class AutoCastDemo {
+
+    public static void takeLong(long number){
+        System.out.println(number);
+    }
+
+    public static void main(String[] args){
+        takeLong(0);
+        takeLong(new Integer(0));
+        final int aDefiniteInteger = 0;
+        takeLong(aDefiniteInteger);
+    }
+}
---
+++ src/main/java/PseudoListInitializer.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by paulbaker on 5/27/15.
+ */
+public class PseudoListInitializer {
+    public static <T> ArrayList<T> AsArrayList(T... items){
+        ArrayList<T> list = new ArrayList<T>(items.length);
+        for(int i = 0; i < items.length; i++){
+            list.add(i, items[i]);
+        }
+        return list;
+    }
+
+    public static <T> LinkedList<T> AsLinkedList(T... items){
+        LinkedList<T> list = new LinkedList<T>();
+        for(int i = 0; i < items.length; i++){
+            list.addLast(items[i]);
+        }
+        return list;
+    }
+
+    public static void main(String[] args){
+        List<String> stringArrayList = AsArrayList("Hello", "My", "Name", "Is", "Sangye Dorje Konchok");
+        List<String> stringLinkedList = AsLinkedList("It", "Is", "A", "Tibetan", "Name");
+    }
+}
