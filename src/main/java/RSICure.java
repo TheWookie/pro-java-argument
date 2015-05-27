3645d1902e09834b6bfe60ac8ced7be577f87f97
Paul Baker <paul.n.baker@utah.edu>
2015-05-27 03:45:16 -0600

    Adding links to vim plugins

---
+++ src/main/java/RSICure.java
@@ -0,0 +1,9 @@
+/**
+ * Created by paulbaker on 5/27/15.
+ */
+public class RSICure {
+    public static void main(String[] args) {
+        System.out.println("https://plugins.jetbrains.com/plugin/164");
+        System.out.println("http://marketplace.eclipse.org/content/vrapper-vim");
+    }
+}
