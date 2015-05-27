f66d090077b5514c85c82657c2f2ced910c6e4ad
Paul Baker <paul.n.baker@utah.edu>
2015-05-27 02:40:48 -0600

    More comments

--- src/main/java/IterableFile.java
+++ src/main/java/IterableFile.java
@@ -26,11 +26,15 @@ public class IterableFile extends File implements Iterable<String> {
             try {
                 scanner = new Scanner(file);
             } catch (FileNotFoundException e) {
+                // This is one of the points where I actually agree.
+                // At times you need to just throw a RuntimeException
+                // instead because there shouldn't be a recovery.
                 throw new RuntimeException(e);
             }
         }
 
         public boolean hasNext() {
+            // We check the has-next and close it gracefully.
             if (scanner.hasNextLine() == false) {
                 scanner.close();
                 return false;
@@ -49,7 +53,7 @@ public class IterableFile extends File implements Iterable<String> {
         // Now we iterate over the file.
         // Note, that this demo shows that java DOES in fact use an interator in a for-each loop
         // Note, this demo also shows a valid use case for using the hasnext method (we close the file
-        // gracefully)
+        // gracefully and silently at the same time)
         for (String fileline : new IterableFile(filename)) {
             System.out.println(fileline);
         }
