c11eb49e7ffcceb9c097dad4e8a95f9f481c07c7
Paul Baker <paul.n.baker@utah.edu>
2015-05-27 03:49:47 -0600

    Adding more comments

--- src/main/java/RSICure.java
+++ src/main/java/RSICure.java
@@ -3,6 +3,18 @@
  */
 public class RSICure {
     public static void main(String[] args) {
+        /*
+         * I do love to use vim. I love it a lot. It isn't necessarily a cure all for
+         * every situation. While it isn't necessary to use an ide, as the original
+         * argument stated, "Haven't we ALREADY PROVED that the CPU can do all the work
+         * in making things easy for us, the user, A LONG TIME AGO?!?". Most IDEs
+         * come with hot-keys to generate 90% of the boilerplate code for us.
+         *
+         * Why type it out manually, when you can have your machine do it for you?
+         * None the less, why not have the best of both worlds? VIM plugins will
+         * allow your IDE to emulate vim while allowing you too keep all the bonus
+         * features of using an IDE.
+         */
         System.out.println("https://plugins.jetbrains.com/plugin/164");
         System.out.println("http://marketplace.eclipse.org/content/vrapper-vim");
     }
